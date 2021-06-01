6bbc08a51c42c368e64142072ab18dd751ca41d0
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-06-01 23:55:17 +0300

    Update __plugin.py

--- userbot/modules/__plugin.py
+++ userbot/modules/__plugin.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# @Qulec tarafından yazılmıştır.
+# @EfsaneStar tarafından yazılmıştır.
 import re
 import os
 from telethon.tl.types import DocumentAttributeFilename, InputMessagesFilterDocument
